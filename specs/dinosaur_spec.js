a5b1bbe3cb1337739b7a6afbd42f6f8ab9b07a3a
Jamie Martin <jamiemartinmusic@gmail.com>
2017-07-25 19:11:04 +0100

    should have a number of offspring dinosaur test passed

--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -4,11 +4,16 @@ var Dinosaur = require('../dinosaur');
 describe('Dinosaur', function(){
   var dinosaur;
 
-beforeEach(function(){
-  dinosaur = new Dinosaur("Dilophosaurus",4)
-});
+  beforeEach(function(){
+    dinosaur = new Dinosaur("Dilophosaurus",4)
+  });
 
-it("should have a type", function(){
-  assert.strictEqual("Dilophosaurus", dinosaur.type);
-});
+  it("should have a type", function(){
+    assert.strictEqual("Dilophosaurus", dinosaur.type);
+  });
+
+
+  it("should have a number of offspring" , function(){
+    assert.strictEqual(4, dinosaur.offspring);
+  });
 });
